f81671816d068b5385a753f7d715fb24a77831b5
Your Name <you@example.com>
2023-04-01 09:47:06 +0300

    new version

---
+++ lib/Action/Help.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Rodzeta\Siteoptions\Action;
+
+final class Help extends Base
+{
+	private $commands;
+	private $action;
+
+	public function getDescription()
+	{
+		return 'bx ' . $this->getName() . ' [command] - Выводит информацию по поддерживаемым командам';
+	}
+
+	protected function printCommandList()
+	{
+		echo $this->getDescription() . "\n";
+
+		foreach ($this->commands as $name => $command)
+		{
+			$commandDescription = 'bx ' . mb_strtolower($name);
+			echo $commandDescription . "\n";
+		}
+	}
+
+	protected function printCommand()
+	{
+		$command = !empty($this->commands[$this->action])? $this->commands[$this->action] : [];
+
+		if (empty($command))
+		{
+			$this->printCommandList();
+			return;
+		}
+
+		if (!empty($command['descr']))
+		{
+			$commandDescription = $command['descr'];
+		}
+		else if (!empty($command['class']))
+		{
+			$commandDescription = $this->create($command['class'])->getDescription();
+		}
+
+		if ($commandDescription != '')
+		{
+			echo $commandDescription . "\n";
+		}
+	}
+
+
+	public function run()
+	{
+		$this->action = $this->params[0] ?? '';
+		$this->commands = $this->getCommands();
+
+		if (empty($this->action))
+		{
+			$this->printCommandList();
+		}
+		else
+		{
+			$this->printCommand();
+		}
+
+		echo "\n";
+	}
+}
+
---
+++ lib/Action/Siteproxy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Rodzeta\Siteoptions\Action;
+
+final class Siteproxy extends Base
+{
+	public function getName()
+	{
+		return 'site-proxy';
+	}
+
+	public function getDescription()
+	{
+		return 'bx ' . $this->getName() . ' [host] - Установить IP для прокси-сайта';
+	}
+
+	public function run()
+	{
+		$destpath = $this->getSiteConfig();
+		$ip = $this->params[0] ?? '';
+
+		$originalContent = file_get_contents($destpath);
+
+		// patch content
+		$content = "\n";
+		if ($ip != '')
+		{
+			$content = '
+ProxyPreserveHost On
+ProxyPass        /  http://' . $ip . '/
+ProxyPassReverse /  http://' . $ip . '/
+';
+		}
+
+		$re = '{\#bx\-proxy\s+start\s.+?\#bx\-proxy\s+end}s';
+		if (preg_match($re, $originalContent))
+		{
+			// replace
+			$originalContent = preg_replace(
+				$re,
+				"#bx-proxy start$content#bx-proxy end",
+				$originalContent
+			);
+		}
+		else
+		{
+			// add
+			$originalContent = str_replace(
+				'</VirtualHost>',
+				"\n#bx-proxy start$content#bx-proxy end\n\n</VirtualHost>",
+				$originalContent
+			);
+		}
+
+		if (trim($content) == '')
+		{
+			$originalContent = str_replace(
+				"#bx-proxy start$content#bx-proxy end",
+				'',
+				$originalContent
+			);
+		}
+
+		// remove empty lines
+		$originalContent = preg_replace(
+			"{\n{2,}}si",
+			"\n\n",
+			$originalContent
+		);
+
+		$this->patchSiteConfig($destpath, $originalContent);
+	}
+}
